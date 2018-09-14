e0722553a504d1845effe32a3305fc5645a52ea3
thainguyenvann <thaitancang001@gmail.com>
2018-09-14 10:01:50 +0700

    finish abstract_implement

---
+++ src/Thai/dev/Triangle.java
@@ -0,0 +1,20 @@
+package Thai.dev;
+
+    public class Triangle extends Shape {
+	 
+	private int a;
+	private int b;
+	private int c;
+	
+	public Triangle(int a, int b, int c) {
+		
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+ 	@Override
+	public double Dientich() {
+		// TODO Auto-generated method stub
+		return Math.sqrt(((a + b + c) * (a + b - c) * (b + c - a) * (c + a - b)) / 4);
+	}
+}
